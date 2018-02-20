2e8302e1dd3a5e5f39870655b58e3289c829ec3d
Ray Suyoto <ray.suy@icloud.com>
2018-02-20 10:56:32 -0700

    removing uneccesary items

--- rb_golf2.rb
+++ rb_golf2.rb
@@ -1,16 +1,11 @@
 #RPS
 
-def rps
-    puts 'Choose r, p, or s'
-    a = ["r", "p", "s"]
-    b = gets.strip
-    c = a.sample
-    if (b === c)
-        puts 'tie'
-    elsif (b === "r" && c === "p" || b === "s" && c === "r" || b === "p" && c === "s")
-        puts 'loser'
-    elsif (b === "r" && c === "s" || b === "p" && c === "r" || b === "s" && c === "p")
-        puts 'winner'
-    end
-end
-#108
+def r
+    @a = ['Win', 'Lose', 'Tie']
+    puts "R P or S"
+    gets
+    puts @a.sample
+    r 
+end 
+
+#27
