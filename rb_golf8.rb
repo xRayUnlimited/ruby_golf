5e365af7850c08f30efd5aca5939644dee873c9d
Ray Suyoto <ray.suy@icloud.com>
2018-04-29 21:46:53 -0600

    solved #8

--- rb_golf8.rb
+++ rb_golf8.rb
@@ -4,6 +4,13 @@
 # Spaces do not count. 
 # Capitols are not the same as lowercase
 
-def lt
-    
+def love_test
+  t = 0
+  s = gets.strip.gsub(" ", "")
+  y = gets.strip.gsub(" ", "")
+  x = s.length + y.length
+  s.split('').each { |c| y.split('').include?(c) ? t += 1 : nil }
+  l = x / t
+  puts l
 end
+love_test
