c4240a56fb30bc364fe00689d868446f54fa2e65
Ray Suyoto <ray.suy@icloud.com>
2018-05-02 18:35:47 -0600

    finally solve hole 9

--- rb_golf9.rb
+++ rb_golf9.rb
@@ -2,6 +2,15 @@
 # Takes in a list of strings. Program sorts the list non case sensitive, 
 # removes duplicates and returns as hash using position in the list as priority.
 
-def shopping (i)
-
+def shopping_list
+  x = 1
+  s = []
+  i = gets.strip.split(" ").uniq.sort
+  i.each do |l|
+    s << Hash[x, l]
+    x += 1
+  end
+  print s
 end
+
+# yeet
